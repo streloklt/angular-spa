944f68f7d5aee8954ae2205d3fcc604bad69ecbf
Alvaro <aleteliert@gmail.com>
2018-08-09 11:01:39 -0400

    Commit

---
+++ src/app/components/buscar/buscar.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-buscar',
+  templateUrl: './buscar.component.html'
+})
+export class BuscarComponent implements OnInit {
+  heroes: any[] = [];
+  termino: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private _heroesService: HeroesService, private router: Router) {}
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroe(params['termino']);
+
+      console.log(this.heroes);
+    });
+  }
+
+  verHeroe(idx: number) {
+    this.router.navigate(['/heroe', idx]);
+  }
+}
